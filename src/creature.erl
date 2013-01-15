cd47da7aa41b371f0dcab377754d62b0a61a6327
Kristian Ionescu <ionescu.kristian@gmail.com>
2013-01-15 14:43:55 +0100

    Lagt till calc_distance och hjälpfunktionen find_way som ska beräkna distansen som en eller flera fiender har från alla neighbors.Det som returneras då är en lista av element som är strukturerad på sådant vis {{X,Y}, Distance} där koordinaterna säger vilken plats som har distance "Distance" från fienderna. , Har även en quicksort funktion som sorterar listan taget från calc_distance och sorterar dessa i fallande ordning utifrån distance värdet., Ska nu jobba på choice lite mer...

--- src/creature.erl
+++ src/creature.erl
@@ -1,9 +1,40 @@
 -module(creature).
 -extends(object).
--export([move/3, reproduce/2, eat/2]).
+-define(HUNGRY, 4).
+-export([move/3, reproduce/2, eat/2, find_way/3, calc_distance/3, qsort/1]).
+
+qsort([]) -> [];
+qsort([ {Coor,D} | T]) -> 
+   qsort([ {Coor,D1} || {Coor,D1} <- T, D1 > D ]) ++ [{Coor, D}] ++ qsort([ {Coor,D1} || {Coor,D1} <- T, D1 =< D ]).
+
+calc_distance(_Paths, [], Result) -> Result;
+calc_distance(Paths, [EH|ET], Result) ->
+    calc_distance(Paths, ET, find_way(Paths,EH,Result)).
+
+find_way([],_Enemy,_Res) -> [];
+find_way([PH|PT],Enemy,[]) ->
+    {{X1,Y1}, _} = PH,
+    {{X2,Y2}, _} = Enemy,
+    Distance = erlang:round(math:sqrt(math:pow((X2 - X1),2) + math:pow((Y2 - Y1),2))),
+    [{{X1,Y1},Distance} | find_way(PT, Enemy, [])];
+find_way([PH|PT],Enemy,[HR|TR]) ->
+    {{X1,Y1}, _} = PH,
+    {{X2,Y2}, _} = Enemy,
+    Distance = erlang:round(math:sqrt(math:pow((X2 - X1),2) + math:pow((Y2 - Y1),2))),
+    {{_X,_Y}, Old_Dis} = HR,
+    case Old_Dis > Distance of
+        true -> [{{X1,Y1},Distance} | find_way(PT, Enemy, TR)];
+        false -> [{{X1,Y1},Old_Dis} | find_way(PT, Enemy, TR)]  
+    end. 
+
+%choice(State,Module, Food, Enemies) ->
+%{Coordinate, Sight, Speed, Hunger, Age, Repro} = State,
+%Neighbors = creature:get_neighbours(Coordinate, Sight),
+%Empty = creature:get_all_empty(Neighbors),
+%Enemy_present = creature:get_of_types(Neighbors, Enemies),
+%Food_present = creature:get_of_types(Neighbors, Food).
 
 
-% tries to move. returns new coordinate upon success.
 move(Coordinate, _Object, 0) -> Coordinate;
 move(Coordinate, Object, Speed) ->
     NewCoordinate = move(Coordinate, Object),
